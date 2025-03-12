a012119fd464172dc1d8508d80a3da76e1d6ea8f
Administrator <tobebestchen@163.com>
2025-03-12 18:00:58 +0800

    20250312-1800

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: '0.0.0.0', // 添加这一行以允许外部访问
+    port: 5173, // 可选：如果你需要指定端口的话
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
-  }
-}) 
+        changeOrigin: true,
+      },
+    },
+  },
+})
