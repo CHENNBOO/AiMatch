b34d676207949767e8bd7ece496d565455517db3
Chen <tobebestchen@163.com>
2025-03-15 00:36:40 +0800

    0037

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import PersonalityMatch from '../components/PersonalityMatch.vue'
-import EventPlanner from '../components/EventPlanner.vue'
-import MentalHealth from '../components/MentalHealth.vue'
-import CommunicationGuide from '../components/CommunicationGuide.vue'
 import VirtualInteraction from '../components/VirtualInteraction.vue'
 
 const routes = [
@@ -16,21 +13,6 @@ const routes = [
     path: '/personality',
     name: '性格匹配',
     component: PersonalityMatch
-  },
-  {
-    path: '/event-planner',
-    name: 'event-planner',
-    component: EventPlanner
-  },
-  {
-    path: '/mental-health',
-    name: 'MentalHealth',
-    component: MentalHealth
-  },
-  {
-    path: '/communication-guide',
-    name: 'communication-guide',
-    component: CommunicationGuide
   },
   {
     path: '/virtual-interaction',
